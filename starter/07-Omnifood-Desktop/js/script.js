b30b35b6ecbb50a26433841c261b2c464f3a82bd
Zangeronimo <zangeronimo@gmail.com>
2022-12-04 14:59:47 -0300

    feat: Implementing Smooth Scrolling

--- starter/07-Omnifood-Desktop/js/script.js
+++ starter/07-Omnifood-Desktop/js/script.js
@@ -30,6 +30,35 @@ btnMobileNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
 
+/////////////////////////////////////////////////////////////////////////////
+// Smooth scrolling animation
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const section = document.querySelector(href);
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.toggle("nav-open");
+    }
+  });
+});
+
 // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
 
 /*
